815295547bdc01774348cd3b4d9f4b888b32b140
Oritsejolomisan Afinotan <sampdoria212@gmail.com>
2022-10-30 21:43:23 +0100

    links updated target

--- src/components/links.js
+++ src/components/links.js
@@ -29,11 +29,11 @@ function Links() {
     return(
         <div className="links-container">
               <a href=""  className="links">Twitter Links</a>
-            <a href="https://training.zuri.team/" id="btn__zuri" className="links">Zuri Team</a>
-            <a href="http://books.zuri.team" id="books" className="links linktip" title='Get Books on Design and Coding from Zuri'>Zuri Books <span className='tooltiptext'>Get Books on Design and Coding from Zuri</span></a>
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=jolomisan" id="book__python" title='Buy our Python For Beginners" book' className="links linktip">Python Books <span className='tooltiptext'>Buy our Python For Beginners' book</span></a>
-            <a href="https://background.zuri.team" id="pitch" title='Our Background Checks Service is bound to fulfil your expectations' className="links linktip">Background Checks for Coders <span className='tooltiptext'>Our Background Checks Service is bound to fulfil your expectations</span></a>
-            <a href="https://books.zuri.team/design-rules " id="book__design" className="links linktip" title='Get our Design Books'> Design Books <span className='tooltiptext'>Get our Design Books</span></a>
+            <a href="https://training.zuri.team/" id="btn__zuri" target="_blank" className="links">Zuri Team</a>
+            <a href="http://books.zuri.team" id="books" className="links linktip" target="_blank" title='Get Books on Design and Coding from Zuri'>Zuri Books <span className='tooltiptext'>Get Books on Design and Coding from Zuri</span></a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=jolomisan" target="_blank" id="book__python" title='Buy our Python For Beginners" book' className="links linktip">Python Books <span className='tooltiptext'>Buy our Python For Beginners' book</span></a>
+            <a href="https://background.zuri.team" id="pitch" target="_blank" title='Our Background Checks Service is bound to fulfil your expectations' className="links linktip">Background Checks for Coders <span className='tooltiptext'>Our Background Checks Service is bound to fulfil your expectations</span></a>
+            <a href="https://books.zuri.team/design-rules " id="book__design" target="_blank" className="links linktip" title='Get our Design Books'> Design Books <span className='tooltiptext'>Get our Design Books</span></a>
 
         <div className="socials">
             <a href=""><img src={slack} alt="" id="slack"/></a>
